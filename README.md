5082dae70c23ca5cb4fc5f58532035c6e10b453b
daliayan <daliayan5@gmail.com>
2021-10-06 17:51:13 -0400

    created models / controllers backend

---
+++ README.md
@@ -0,0 +1,1 @@
+This is where my readme file will be.
---
+++ backend/db/migrate/20211006214858_create_banks.rb
@@ -0,0 +1,10 @@
+class CreateBanks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :banks do |t|
+      t.string :name
+      t.integer :amount
+
+      t.timestamps
+    end
+  end
+end
